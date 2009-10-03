7efc19c01938cb14d0eb07c25356489514d26301
Brandon Dimcheff <bdimchef-git@wieldim.com>
2009-10-03 02:21:58 -0400

    better demo file

--- benchmark.rb
+++ benchmark.rb
@@ -1,6 +1,17 @@
 require 'benchmark'
 require 'enumerator'
 
+list = (1..100000).map { rand(100000) }
+
+puts
+puts "sort and sort_by with fast comparisons:"
+gets
+
+Benchmark.bm(10) do |b|
+  b.report("sort")    { list.dup.sort }
+  b.report("sort_by") { list.dup.sort_by {|x| x } }
+end
+
 class Foo
   attr_accessor :val
   
@@ -11,34 +22,19 @@ end
 
 list = (1..100000).map {|i| 
   Foo.new(i)
-}
+}.sort_by { rand }
 
 puts
-puts "Fast comparisons:"
+puts "sort and sort_by with slow comparisons:"
 gets
 
+# Even something as simple as method dispatch is slow enough to make sort_by faster than sort
 Benchmark.bm(10) do |b|
-  b.report("Sort")    { list.dup.sort {|x, y| x.val <=> y.val} }
-  b.report("Sort by") { list.dup.sort_by {|x| x.val } }
+  b.report("sort")    { list.dup.sort {|x, y| x.val <=> y.val} }
+  b.report("sort_by") { list.dup.sort_by {|x| x.val } }
 end
 
-class Foo
-  def val
-    # do something superfluous
-    (1..10).map {|i| i**i}
-    @val
-  end
-end
-
-puts
-puts "Slow comparisons:"
-gets
-
-Benchmark.bm(10) do |b|
-  b.report("Sort")    { list.dup.sort {|x, y| x.val <=> y.val} }
-  b.report("Sort by") { list.dup.sort_by {|x| x.val } }
-end
-
+# Make foo print a message whenever the accessor is called
 class Foo
   def val
     puts "calling val: #{@val}"
@@ -61,6 +57,8 @@ gets
 list.dup.sort_by {|x| x.val }
 
 module Enumerable
+  # This is essentially the implementation of sort_by
+  # It's written in C in MRI, so it's a little faster
   def my_sort_by
     self.map {|i| [yield(i), i]}.sort.map {|j| j[1]}
   end
@@ -72,24 +70,24 @@ gets
 
 list.dup.my_sort_by {|x| x.val }
 
-list = (1..100000).map {|i| Foo.new(i)}
+list = (1..100000).map {|i| Foo.new(i)}.sort_by { rand }
 
 class Foo
   def val
     # do something superfluous
-    (1..10).map {|i| i**i}
+    (1..5).map {|i| i**i}
     @val
   end
 end
 
 puts
-puts "my_sort_by with slow comparisons:"
+puts "sort, sort_by, and my_sort_by with slow comparisons:"
 gets
 list.dup.my_sort_by {|x| x.val }
 
 Benchmark.bm(10) do |b|
-  b.report("Sort")    { list.dup.sort {|x, y| x.val <=> y.val} }
-  b.report("Sort by") { list.dup.sort_by {|x| x.val } }
-  b.report("My sort by") { list.dup.my_sort_by {|x| x.val } }
+  b.report("sort")    { list.dup.sort {|x, y| x.val <=> y.val} }
+  b.report("sort_by") { list.dup.sort_by {|x| x.val } }
+  b.report("my_sort_by") { list.dup.my_sort_by {|x| x.val } }
 end
 
